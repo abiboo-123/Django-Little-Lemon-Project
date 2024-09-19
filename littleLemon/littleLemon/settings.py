d89168445684c0d91382fc2cd0030394a065c5e4
Habib <habubyies@gmail.com>
2024-09-19 20:38:09 +0300

    Models (booking, Menu) and Mysql Configrations

--- littleLemon/littleLemon/settings.py
+++ littleLemon/littleLemon/settings.py
@@ -76,10 +76,18 @@ WSGI_APPLICATION = 'littleLemon.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': 
+        {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'LittleLemon',
+            'USER': 'root',
+            'PASSWORD': '6624',
+            'HOST': '127.0.0.1',
+            'PORT': '3306',
+            'OPTIONS': { 
+                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+            }
+        }
 }
 
 
@@ -124,4 +132,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
