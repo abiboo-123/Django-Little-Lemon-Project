30276a6e006477b2738179d2a2bfb0a18d8c1149
Habib <habubyies@gmail.com>
2024-09-20 18:04:59 +0300

    Testing

--- littleLemon/restaurant/tests.py
+++ littleLemon/restaurant/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from django.test import TestCase  
+from .models import MenuItem
 
-# Create your tests here.
+class MenuItemTest(TestCase):
+    def test_get_item(self):
+        # Create a MenuItem object
+        item = MenuItem.objects.create(Title="IceCream", Price=80, Inventory=100)
+        
+        # Check if the string representation (__str__) is correct
+        self.assertEqual(str(item), 'IceCream : 80')
